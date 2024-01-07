5d26a0fbb4595a4b4242d8dbb0c37ebbe5d5ae5e
MatheuBotha <matheu.botha@gmail.com>
2024-01-07 15:28:40 +0200

    finished diamond printer

--- diamond.py
+++ diamond.py
@@ -1,20 +1,27 @@
+from typing import List
 from pprint import pprint
 
 def rows(letter):
     diamond = []
-    radius = ord(letter)-ord('A')
-    print(radius)
-    if radius == 0:
-        return ['A']
-    
-    for idx in range(radius+1):
-        print(idx)
-        s = f'{chr(ord(letter)-idx)}'
-        js = s.rjust(idx+1, ' ')
-        print(js)
-        diamond.append(js)
+    radius = ord(letter)-ord('A')+1
+    for idx in range(radius):
+        current_char = chr(ord('A')+idx)
+        row = f"{current_char}".rjust(radius-idx, ' ')
+        if idx != 0:
+            row += f"{current_char}".rjust(((2*(idx-1))+1)+1)
+        row += ''.join([' ' for _ in range(radius-idx-1)])
+        diamond.append(row)
+    diamond.extend(diamond[:-1][::-1])
     return diamond
 
-print(rows('A'))
-print(rows('B'))
-print(rows('C'))
+def better_rows(letter: str) -> List[str]:
+    letters = [chr(k) for k in range(ord('A'), ord(letter) + 1)]
+    print(letters)
+    alphabet = letters[:-1] + letters[::-1]
+    print(alphabet)
+    diamond_line = letters[::-1] + letters[1:]
+    print(diamond_line)
+    return [''.join(x if x == y else ' ' for y in diamond_line) for x in alphabet]
+
+pprint(rows('E'))
+pprint(better_rows('E'))
